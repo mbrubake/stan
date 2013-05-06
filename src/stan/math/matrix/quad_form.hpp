11dadf6a485aa8ad1798ee18778f57d4579cfc4a
Marcus Brubaker <marcus.a.brubaker@gmail.com>
2013-05-05 20:45:10 -0400

    More quad_form functions.

--- src/stan/math/matrix/quad_form.hpp
+++ src/stan/math/matrix/quad_form.hpp
@@ -9,6 +9,9 @@
 
 namespace stan {
   namespace math {
+    /**
+     * Compute B^T A B
+     **/
     template<typename DerivedA,typename DerivedB>
     inline typename
     boost::enable_if_c< boost::is_same<typename DerivedA::Scalar,double>::value &&
@@ -22,6 +25,9 @@ namespace stan {
       return B.transpose()*A*B;
     }
     
+    /**
+     * Compute B^T A^-1 B
+     **/
     template<typename DerivedA,typename DerivedB>
     inline typename
     boost::enable_if_c< boost::is_same<typename DerivedA::Scalar,double>::value &&
@@ -35,17 +41,56 @@ namespace stan {
       return B.transpose()*A.qr().solve(B);
     }
     
+    /**
+     * Compute B^T A^-1 B where A is a symmetric, positive definite matrix.
+     **/
     template<typename DerivedA,typename DerivedB>
     inline typename
     boost::enable_if_c< boost::is_same<typename DerivedA::Scalar,double>::value &&
-                        boost::is_same<typename DerivedB::Scalar,double>::value,
-                        Eigen::Matrix<double,DerivedB::ColsAtCompileTime,DerivedB::ColsAtCompileTime> >::type
+    boost::is_same<typename DerivedB::Scalar,double>::value,
+    Eigen::Matrix<double,DerivedB::ColsAtCompileTime,DerivedB::ColsAtCompileTime> >::type
     inv_quad_form_spd(const Eigen::MatrixBase<DerivedA> &A,
                       const Eigen::MatrixBase<DerivedB> &B)
     {
       validate_square(A,"inv_quad_form");
       validate_multiplicable(A,B,"inv_quad_form");
       return B.transpose()*A.ldlt().solve(B);
+    }
+    
+    /**
+     * Compute trace(B^T A^-1 B).
+     **/
+    template<typename DerivedD, typename DerivedA,typename DerivedB>
+    inline typename
+    boost::enable_if_c< boost::is_same<typename DerivedA::Scalar,double>::value &&
+                        boost::is_same<typename DerivedB::Scalar,double>::value,
+                        double >::type
+    trace_inv_quad_form(const Eigen::MatrixBase<DerivedA> &A,
+                        const Eigen::MatrixBase<DerivedB> &B)
+    {
+      validate_square(A,"trace_inv_quad_form");
+      validate_multiplicable(A,B,"trace_inv_quad_form");
+      return (B.transpose()*A.qr().solve(B)).trace();
+    }
+    
+    /**
+     * Compute trace(D^-1 B^T A^-1 B).
+     **/
+    template<typename DerivedD, typename DerivedA,typename DerivedB>
+    inline typename
+    boost::enable_if_c< boost::is_same<typename DerivedD::Scalar,double>::value &&
+                        boost::is_same<typename DerivedA::Scalar,double>::value &&
+                        boost::is_same<typename DerivedB::Scalar,double>::value,
+                        double >::type
+    trace_inv_quad_form(const Eigen::MatrixBase<DerivedB> &D,
+                        const Eigen::MatrixBase<DerivedA> &A,
+                        const Eigen::MatrixBase<DerivedB> &B)
+    {
+      validate_square(A,"trace_inv_quad_form");
+      validate_square(D,"trace_inv_quad_form");
+      validate_multiplicable(A,B,"trace_inv_quad_form");
+      validate_multiplicable(B,D,"trace_inv_quad_form");
+      return D.qr().solve(B.transpose()*A.qr().solve(B)).trace();
     }
   }
 }
