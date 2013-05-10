06de56610e961115abe7800c276535e66c693933
Marcus Brubaker <marcus.a.brubaker@gmail.com>
2013-05-10 13:27:22 -0400

    Implement agrad/rev version of quad_form.

--- src/stan/gm/function_signatures.hpp
+++ src/stan/gm/function_signatures.hpp
@@ -149,6 +149,8 @@ add("squared_dist",DOUBLE_T,ROW_VECTOR_T,ROW_VECTOR_T);
 add("squared_dist",DOUBLE_T,VECTOR_T,ROW_VECTOR_T);
 add("squared_dist",DOUBLE_T,ROW_VECTOR_T,VECTOR_T);
 add("squared_dist",DOUBLE_T,MATRIX_T,MATRIX_T);
+add("quad_form",DOUBLE_T,MATRIX_T,VECTOR_T);
+add("quad_form",MATRIX_T,MATRIX_T,MATRIX_T);
 add("dot_product",DOUBLE_T,VECTOR_T,VECTOR_T);
 add("dot_product",DOUBLE_T,ROW_VECTOR_T,ROW_VECTOR_T);
 add("dot_product",DOUBLE_T,VECTOR_T,ROW_VECTOR_T);
